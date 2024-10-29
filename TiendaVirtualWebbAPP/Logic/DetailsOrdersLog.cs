c36bca4082ce426d23adf71284efa91ebf7efb2a
Jehan Vente <jventequinones@gmail.com>
2024-10-29 17:15:29 -0500

    se crea el ddl en la capa data y la capa logic para la clase detalles de orden

---
+++ TiendaVirtualWebbAPP/Logic/DetailsOrdersLog.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Logic
+{
+    public class DetailsOrdersLog
+    {
+        OrderDat objord = new OrderDat();
+
+        public DataSet showDetailsOrders()
+        {
+            return objord.showDetailsOrders();
+        }
+
+        public bool saveDetailsOrder(int _detCantidad, double _detPrecio, int _fkPedido, int _fkProducto)
+        {
+            return objord.saveDetailsOrder( _detCantidad, _detPrecio, _fkPedido, _fkProducto);
+        }
+
+        public bool updateDetailsOrder(int _detId, int _detCantidad, double _detPrecio, int _fkPedido, int _fkProducto)
+        {
+            return objord.updateDetailsOrder( _detId, _detCantidad, _detPrecio, _fkPedido, _fkProducto);
+        }
+        public bool deleteDetailsOrder(int _detId)
+        {
+            return objord.deleteDetailsOrder(_detId);
+        }
+        public DataSet procShowDetallesPedidoDDL()
+        {
+            return objord.procShowDetallesPedidoDDL();
+        }
+    }
+}
