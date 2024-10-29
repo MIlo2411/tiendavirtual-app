fe404e2b287cec135665dbae76ae906b3b03a2e3
Jehan Vente <jventequinones@gmail.com>
2024-10-29 17:08:26 -0500

    se crea el ddl en la capa data y la capa logic para la clase categoria

---
+++ TiendaVirtualWebbAPP/Logic/CategoryLog.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Logic
+{
+    public class CategoryLog
+    {
+        OrderDat objord = new OrderDat();
+
+        public DataSet showCategories()
+        {
+            return objord.showCategories();
+        }
+
+        public bool saveCategory(string nombre, string descripcion)
+        {
+            return objord.saveCategory(_id, _fecha, _estado, _total, _clientes_cli_id);
+        }
+
+        public bool updateCategory(int id, string nombre, string descripcion)
+        {
+            return objord.updateCategory( id, nombre, descripcion);
+        }
+        public bool deleteCategory(int _id)
+        {
+            return objord.deleteCategory(_id);
+        }
+        public DataSet procShowCategoryDDL()
+        {
+            return objord.procShowCategoryDDL();
+        }
+
+    }
+}
+}
